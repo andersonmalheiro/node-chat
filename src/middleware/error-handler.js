4ac269009beac38f1061228866ee24b7304a8798
Anderson Malheiro de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2019-08-24 20:50:19 -0300

    Adding error handler middleware

---
+++ src/middleware/error-handler.js
@@ -0,0 +1,22 @@
+const { APIError, InternalServerError } = require("rest-api-errors");
+const { STATUS_CODES } = require("http");
+
+const errorHandler = (err, req, res, next) => {
+  console.log(err);
+  const error =
+    err.status === 401 || err instanceof APIError
+      ? err
+      : new InternalServerError();
+
+  // Mongoose errors
+  if (["ValidationError", "UserExistsError"].includes(err.name)) {
+    return res.status(405).json(err);
+  }
+
+  return res.status(error.status || 500).json({
+    code: error.code || 500,
+    message: error.message || STATUS_CODES[error.status]
+  });
+};
+
+module.exports = errorHandler;
