db6fc6536475a4154294941b12797ca0decbaa59
Anderson Malheiro de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2019-08-24 20:53:50 -0300

    Refactoring get and list user to not return password

--- src/controllers/user/get.js
+++ src/controllers/user/get.js
@@ -3,7 +3,7 @@ const get = ({ User }, { config }) => async (req, res, next) => {
   const { _id } = req.params;
   try {
     // Busca o usuario
-    const user = await User.findOne({ _id });
+    const user = await User.findById({ _id }).select("-password");
     res.status(200).send({ user });
   } catch (error) {
     // Envia o erro para o handler de erros
@@ -11,4 +11,4 @@ const get = ({ User }, { config }) => async (req, res, next) => {
   }
 };
 
-module.exports = { get };
+module.exports = get;
--- src/controllers/user/list.js
+++ src/controllers/user/list.js
@@ -1,7 +1,7 @@
 const list = ({ User }, { config }) => async (req, res, next) => {
   try {
     // Busca todos os usuários
-    const users = await User.find({});
+    const users = await User.find({}).select("-password");
     // Retorna os usuários
     res.status(200).send({ users });
   } catch (error) {
@@ -9,4 +9,4 @@ const list = ({ User }, { config }) => async (req, res, next) => {
   }
 };
 
-module.exports = { list };
+module.exports = list;
